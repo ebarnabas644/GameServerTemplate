f495a9ada6aa0e7c9fa29d3c5b601ad01ab81e04
ebarnabas <macskabal2000@gmail.com>
2023-11-26 19:50:45 +0100

    Main hub added

---
+++ Hubs/MainHub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace ProjectRPS.Hubs;
+
+public partial class MainHub : Hub
+{
+    public override async Task OnConnectedAsync()
+    {
+        var id = Context.ConnectionId;
+        await Clients.Client(Context.ConnectionId).SendAsync("initial", "Hello there");
+    }
+}
