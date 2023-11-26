3554485fc1c901f7518b46670b1a204a8aa2e8c1
ebarnabas <macskabal2000@gmail.com>
2023-11-26 19:51:11 +0100

    Basic entity and component added

---
+++ Core/Entity/Components/Component.cs
@@ -0,0 +1,16 @@
+﻿namespace ProjectRPS.Core.Components;
+
+public abstract class Component
+{
+    protected Entity _entity;
+    
+    public void Attach(Entity entity)
+    {
+        _entity = entity;
+    }
+
+    public void Detach(Entity entity)
+    {
+        _entity = null;
+    }
+}
---
+++ Core/Entity/Entity.cs
@@ -0,0 +1,33 @@
+﻿using MathNet.Numerics.LinearAlgebra;
+using MathNet.Numerics.LinearAlgebra.Double;
+using ProjectRPS.Core.Components;
+
+namespace ProjectRPS.Core;
+
+public class Entity
+{
+    public Vector<double> Position;
+    private readonly Dictionary<string, Component> _components;
+
+    public Entity()
+    {
+        _components = new Dictionary<string, Component>();
+        Position = new DenseVector(new double[] { 2, 0 });
+    }
+
+    public void AddComponent(string name, Component component)
+    {
+        component.Attach(this);
+        _components[name] = component;
+    }
+
+    public void RemoveComponent(string name)
+    {
+        _components.Remove(name);
+    }
+
+    public Component GetComponent(string name)
+    {
+        return _components[name];
+    }
+}
