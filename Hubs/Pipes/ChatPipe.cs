af9f1093d02a119e29f64e199af5641d62366dd7
ebarnabas <macskabal2000@gmail.com>
2023-11-26 19:50:16 +0100

    Dividing main hub class into partial classes

---
+++ Hubs/Pipes/ChatPipe.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace ProjectRPS.Hubs;
+
+public partial class MainHub : Hub
+{
+    public async Task SendMessage(string message)
+    {
+        await Clients.All.SendAsync("message", message);
+    }
+}
---
+++ Hubs/Pipes/PlayerPipe.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace ProjectRPS.Hubs;
+
+public partial class MainHub : Hub
+{
+    public async Task SendPlayerState(string message)
+    {
+        await Clients.All.SendAsync("message", message);
+    }
+}
