00e4feb260fc4af719ba4e494bee4969dbf03c60
ebarnabas <macskabal2000@gmail.com>
2024-01-04 23:21:31 +0100

    Registering main hub

--- ServiceCollectionExtensions.cs
+++ ServiceCollectionExtensions.cs
@@ -1,9 +1,17 @@
-﻿using ProjectRPS.Core;
+﻿using Microsoft.AspNetCore.SignalR;
+using ProjectRPS.Core;
+using ProjectRPS.Hubs;
 
 namespace ProjectRPS;
 
 public static class ServiceCollectionExtensions
 {
+    public static IServiceCollection AddSignalRHub(this IServiceCollection services)
+    {
+        services.AddSingleton<Hub, MainHub>();
+
+        return services;
+    }
     public static IServiceCollection AddGameLoop(this IServiceCollection services)
     {
         services.AddSingleton<IGameLoop, GameLoop>();
