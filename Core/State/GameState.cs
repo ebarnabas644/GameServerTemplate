e810ea4c8662e15fa1aaf33191ab2d592347c81d
ebarnabas <macskabal2000@gmail.com>
2024-01-04 23:21:42 +0100

    Creating game state

---
+++ Core/State/GameState.cs
@@ -0,0 +1,17 @@
+﻿namespace ProjectRPS.Core.State;
+
+public class GameState
+{
+    private List<Entity> _entities;
+
+    public GameState()
+    {
+        _entities = new List<Entity>();
+    }
+
+    public void CreatePlayerEntity(string id)
+    {
+        
+        
+    }
+}
