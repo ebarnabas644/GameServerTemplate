1c5822d0e1e7a4619e947e041c42bac4b40a9508
ebarnabas <macskabal2000@gmail.com>
2024-01-06 23:07:56 +0100

    Creating Velocity component

--- Core/Entity/Components/VelocityComponent.cs
+++ Core/Entity/Components/VelocityComponent.cs
@@ -1,6 +1,13 @@
-﻿namespace ProjectRPS.Core.Components;
+﻿using ProjectRPS.Core.Models;
+
+namespace ProjectRPS.Core.Components;
 
 public class VelocityComponent
 {
-    
+    public Position Velocity;
+
+    public VelocityComponent()
+    {
+        Velocity = new Position();
+    }
 }
