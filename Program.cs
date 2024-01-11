0d4dfab46c728d875d7981caa1f4d53a29045cad
ebarnabas <macskabal2000@gmail.com>
2024-01-11 23:00:55 +0100

    Removing comments

--- Program.cs
+++ Program.cs
@@ -4,10 +4,7 @@ using ProjectRPS.Hubs;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddSignalR();
@@ -18,7 +15,6 @@ builder.Services.AddMessageProcessors();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
