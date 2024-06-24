0569f41c7329d2006bb01a3cb2ed05b3cb99525e
ebarnabas <macskabal2000@gmail.com>
2024-06-24 22:07:24 +0200

    Changing entity speed on button press testing

--- Network/MessageProcessors/InputProcessor.cs
+++ Network/MessageProcessors/InputProcessor.cs
@@ -1,20 +1,30 @@
 ﻿using System.Text.Json;
+using ProjectRPS.Core.Components;
 using ProjectRPS.Core.Models;
+using ProjectRPS.Core.State;
 
 namespace ProjectRPS.Hubs.MessageProcessors;
 
 public class InputProcessor : IMessageProcessor
 {
     private readonly ILogger<InputProcessor> _logger;
+    private readonly IGameState _gameState;
     
-    public InputProcessor(ILogger<InputProcessor> logger)
+    public InputProcessor(ILogger<InputProcessor> logger, IGameState gameState)
     {
         _logger = logger;
+        _gameState = gameState;
     }
     
     public async Task Process(string data)
     {
         var parsed = JsonSerializer.Deserialize<List<KeyInput>>(data);
+        var player = _gameState.GetGameState().FirstOrDefault();
+        if (player != null)
+        {
+            var velocity = player.GetComponent("Velocity") as VelocityComponent;
+            velocity.Velocity.X += 1;
+        }
         _logger.LogInformation($"Input received: {string.Join(',', parsed.Select(x => x.Input))}");
     }
 
