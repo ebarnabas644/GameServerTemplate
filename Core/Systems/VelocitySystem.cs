0538cfaf09f7b1f15f553f16b29cc84efc891125
ebarnabas <macskabal2000@gmail.com>
2024-06-24 22:06:29 +0200

    Basic velocity system implementation added

--- Core/Systems/VelocitySystem.cs
+++ Core/Systems/VelocitySystem.cs
@@ -1,13 +1,25 @@
-﻿namespace ProjectRPS.Core.Systems;
+﻿using ProjectRPS.Core.Components;
+using ProjectRPS.Core.State;
+
+namespace ProjectRPS.Core.Systems;
 
 public class VelocitySystem : ISystem
 {
-    public VelocitySystem()
+    private readonly IGameState _gameState;
+    
+    public VelocitySystem(IGameState gameState)
     {
-        
+        _gameState = gameState;
     }
     public void Process()
     {
-        
+        foreach (var entity in _gameState.GetGameState())
+        {
+            if (entity.GetComponent("Velocity") is VelocityComponent velocityComponent &&
+                entity.GetComponent("Position") is PositionComponent positionComponent)
+            {
+                positionComponent.Vector.Add(velocityComponent.Velocity);
+            }
+        }
     }
 }
