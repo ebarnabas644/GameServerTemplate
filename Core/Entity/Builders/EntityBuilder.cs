005aec63291e8058812283af5a408fc225706504
ebarnabas <macskabal2000@gmail.com>
2024-07-26 23:54:24 +0200

    Adding basic entity factory

---
+++ Core/Entity/Builders/EntityBuilder.cs
@@ -0,0 +1,47 @@
+﻿using ProjectRPS.Core.Components;
+
+namespace ProjectRPS.Core.Builders;
+
+public interface IEntityBuilder
+{
+    Entity BuildPlayer(string? connectionId);
+    Entity BuildMob();
+}
+
+public class EntityBuilder : IEntityBuilder
+{
+    public Entity BuildPlayer(string? connectionId)
+    {
+        var entity = new Entity();
+        var positionComponent = new PositionComponent();
+        var random = new Random();
+        positionComponent.Vector.X = random.Next() % 500;
+        positionComponent.Vector.Y = random.Next() % 500;
+        entity.AddComponent("Position", positionComponent);
+        entity.AddComponent("Velocity", new VelocityComponent());
+        entity.AddComponent("State", new StateComponent { State = "Idle" });
+        entity.AddComponent("Sprite", new SpriteComponent { Sprite = "player.png" });
+        
+        if (connectionId != null)
+        {
+            entity.ConnectionId = connectionId;
+        }
+
+        return entity;
+    }
+
+    public Entity BuildMob()
+    {
+        var entity = new Entity();
+        var positionComponent = new PositionComponent();
+        var random = new Random();
+        positionComponent.Vector.X = 200 + random.Next() % 500;
+        positionComponent.Vector.Y = 200 + random.Next() % 500;
+        entity.AddComponent("Position", positionComponent);
+        entity.AddComponent("Velocity", new VelocityComponent());
+        entity.AddComponent("State", new StateComponent { State = "Idle" });
+        entity.AddComponent("Sprite", new SpriteComponent { Sprite = "slime.png" });
+
+        return entity;
+    }
+}
--- Core/State/GameState.cs
+++ Core/State/GameState.cs
@@ -4,8 +4,7 @@ namespace ProjectRPS.Core.State;
 
 public interface IGameState
 {
-    Guid CreatePlayerEntity(string? connectionId);
-    Guid CreateMobEntity();
+    void AddEntity(Entity entity);
     void DeletePlayerEntity(string connectionId);
     List<Entity> GetGameState();
 }
@@ -19,42 +18,9 @@ public class GameState : IGameState
         _entities = new List<Entity>();
     }
 
-    public Guid CreatePlayerEntity(string? connectionId)
+    public void AddEntity(Entity entity)
     {
-        var entity = new Entity();
-        var positionComponent = new PositionComponent();
-        var random = new Random();
-        positionComponent.Vector.X = random.Next() % 500;
-        positionComponent.Vector.Y = random.Next() % 500;
-        entity.AddComponent("Position", positionComponent);
-        entity.AddComponent("Velocity", new VelocityComponent());
-        entity.AddComponent("State", new StateComponent { State = "Idle" });
-        entity.AddComponent("Sprite", new SpriteComponent { Sprite = "player.png" });
-        
-        if (connectionId != null)
-        {
-            entity.ConnectionId = connectionId;
-        }
         _entities.Add(entity);
-
-        return entity.Id;
-    }
-    
-    public Guid CreateMobEntity()
-    {
-        var entity = new Entity();
-        var positionComponent = new PositionComponent();
-        var random = new Random();
-        positionComponent.Vector.X = 200 + random.Next() % 500;
-        positionComponent.Vector.Y = 200 + random.Next() % 500;
-        entity.AddComponent("Position", positionComponent);
-        entity.AddComponent("Velocity", new VelocityComponent());
-        entity.AddComponent("State", new StateComponent { State = "Idle" });
-        entity.AddComponent("Sprite", new SpriteComponent { Sprite = "slime.png" });
-        
-        _entities.Add(entity);
-
-        return entity.Id;
     }
 
     public void DeletePlayerEntity(string connectionId)
